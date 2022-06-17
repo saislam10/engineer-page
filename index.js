e3e62874fa5c127ac91570583375fbf5996927e0
Safwan Islam <saislam@davidson.edu>
2022-06-17 09:53:25 -0500

    continued trying to cycle through the questions, not done yet

--- index.js
+++ index.js
@@ -8,89 +8,122 @@ const Manager = require("./lib/manager");
 myTeamArray = [];
 
 function runner() {
-    function myTeam () {
+    function manager() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name1',
+                message: 'What is the name of the manager?',
+            },
+            {
+                type: 'input',
+                name: 'id1',
+                message: 'What is the id number of the manager?',
+            },
+            {
+                type: 'input',
+                name: 'email1',
+                message: 'What is the email of the manager?',
+            },
+            {
+                type: 'input',
+                name: 'officeNumber1',
+                message: 'What is the office number of the manager?',
+            },
+        ]).then(input => {
+            const manager = new Manager(input.name1, input.id1, input.email1, input.officeNumber1);
+            myTeamArray.push(manager);
+        });
+    }
+
+    function myTeam() {
         inquirer.prompt([{
             type: "list",
-            
-        }])
+            name: "prompt",
+            message: "What kind of team member would you like to add?",
+            choices: ['Engineer', 'Intern', 'No More']
+        }]).then(function (input) {
+            switch (input.prompt) {
+                case "Engineer":
+                    engineer1();
+                    break;
+                case "Intern":
+                    intern2();
+                    break;
+                default: {
+                    htmlGenerator();
+                };
+            }
+        })
     }
-    function managerArray() {
-        [
-            inquirer.prompt([
-                {
-                    type: 'input',
-                    name: 'name1',
-                    message: 'What is the name of the manager?',
-                },
-                {
-                    type: 'input',
-                    name: 'id1',
-                    message: 'What is the id number of the manager?',
-                },
-                {
-                    type: 'input',
-                    name: 'email1',
-                    message: 'What is the email of the manager?',
-                },
-                {
-                    type: 'input',
-                    name: 'officeNumber1',
-                    message: 'What is the office number of the manager?',
-                },
-            ]).then(answers => {
-                const manager = new Manager(answers.name1, answers.id1, answers.email1, answers.officeNumber1);
-                myTeamArray.push(manager);
+    function engineer1() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name2',
+                message: 'What is the name of the engineer?',
+            },
+            {
+                type: 'input',
+                name: 'id2',
+                message: 'What is the id number of the engineer?',
+            },
+            {
+                type: 'input',
+                name: 'email2',
+                message: 'What is the email of the engineer?',
+            },
+            {
+                type: 'input',
+                name: 'gitHub2',
+                message: 'What is the gitHub username of the engineer?',
+            },
+        ]).then(input => {
+            const engineer = new Engineer(input.name2, input.id2, input.email2, input.gitHub2);
+            myTeamArray.push(engineer);
+            myTeam();
+        });
+    }
 
-            });
+    function intern2() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name3',
+                message: 'What is the name of the intern?',
+            },
+            {
+                type: 'input',
+                name: 'id3',
+                message: 'What is the id number of the intern?',
+            },
+            {
+                type: 'input',
+                name: 'email3',
+                message: 'What is the email of the intern?',
+            },
+            {
+                type: 'input',
+                name: 'school3',
+                message: 'What school did the intern go to?',
+            },
+        ]).then(input => {
+            const intern = new Intern(input.name3, input.id3, input.email3, input.school3);
+            myTeamArray.push(intern);
+            myTeam();
+        })
+    }
+    function htmlGenerator() {
+        fs.writeFileSync(utils.generateHTML(myTeamArray));
+    }
+
+    myTeam();
+
 }
-    const engineerArray = [
-        {
-            type: 'input',
-            name: 'name2',
-            message: 'What is the name of the engineer?',
-        },
-        {
-            type: 'input',
-            name: 'id2',
-            message: 'What is the id number of the engineer?',
-        },
-        {
-            type: 'input',
-            name: 'email2',
-            message: 'What is the email of the engineer?',
-        },
-        {
-            type: 'input',
-            name: 'gitHub2',
-            message: 'What is the gitHub username of the engineer?',
-        },
-    ];
-    inquirer.prompt(engineerArray).then((answers2) => {
-        utils.generateHTML(answers2);
-    })
-    const internArray = [
-        {
-            type: 'input',
-            name: 'name3',
-            message: 'What is the name of the intern?',
-        },
-        {
-            type: 'input',
-            name: 'id3',
-            message: 'What is the id number of the intern?',
-        },
-        {
-            type: 'input',
-            name: 'email3',
-            message: 'What is the email of the intern?',
-        },
-        {
-            type: 'input',
-            name: 'school3',
-            message: 'What school did the intern go to?',
-        },
-    ];
-    inquirer.prompt(internArray).then((answers3) => {
-        utils.generateHTML(answers3);
-    })
-}
+
+runner();
+
+
+
+
+
